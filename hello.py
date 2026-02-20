94d1b0a3578df99040e52ad771f3d18b7232b620
soumi mishra <mishrasoumi2004@gmail.com>
2026-02-20 12:08:45 +0530

    files added

--- hello.py
+++ hello.py
@@ -1,2 +1,14 @@
-print("hi")
-print(5)
+# sum=0
+# for i in range(1,11 ):
+#     sum += i
+# print (sum)
+list= ['f','g','h','g','t']
+# reverse
+print(list[::-1]) # start,end,step
+
+str='jecrc hi'
+print(str.split(' '))
+rev=str[::-1]
+print(rev)
+# or
+ 
